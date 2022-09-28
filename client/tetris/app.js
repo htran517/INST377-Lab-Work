927888c70d7c328401895c194a95d27b5617f8c8
htran517 <htran517@terpmail.umd.edu>
2022-09-28 15:11:31 -0400

    score function

--- client/tetris/app.js
+++ client/tetris/app.js
@@ -6,6 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const width = 10;
   let nextRandom = 0;
   let timerID;
+  let score = 0;
 
   // The Tetrominoes
   const lTetromino = [
@@ -67,7 +68,7 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   // make the tetromino move down every second
-  timerID = setInterval(moveDown, 1000);
+  // timerID = setInterval(moveDown, 1000);
 
   // assign functions to keyCodes
   function control(e) {
@@ -102,6 +103,7 @@ document.addEventListener('DOMContentLoaded', () => {
       currentPosition = 4;
       draw();
       displayShape();
+      addScore();
     }
   }
 
@@ -180,5 +182,24 @@ document.addEventListener('DOMContentLoaded', () => {
       nextRandom = Math.floor(Math.random() * theTetrominoes.length);
       displayShape();
     }
-  })
+  });
+
+  // add score
+  function addScore() {
+    for (let i = 0; i < 199; i += width) {
+      const row = [i, i + 1, i + 2, i + 3, i + 4, i + 5, i + 6, i + 7, i + 8, i + 8];
+
+      if (row.every(index => squares[index].classList.contains('taken'))) {
+        score += 10;
+        scoreDisplay.innerHTML = score;
+        row.forEach(index => {
+          squares[index].classList.remove('taken');
+          squares[index].classList.remove('tetromino');
+        });
+        const squaresRemoved = squares.splice(i, width);
+        squares = squaresRemoved.concat(squares);
+        squares.forEach(cell => grid.appendChild(cell));
+      }
+    }
+  }
 });
