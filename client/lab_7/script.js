37034f8d2486ff0bc9800cb2708234369c7d9670
htran517 <htran517@terpmail.umd.edu>
2022-10-27 18:14:07 -0400

    lab7 working

--- client/lab_7/script.js
+++ client/lab_7/script.js
@@ -18,15 +18,15 @@ function getRandomIntInclusive(min, max) {
 }
 
 function injectHTML(list) {
-  console.log("fired injectHTML");
-  const target = document.querySelector("#restaurant_list");
-  target.innerHTML = "";
+  console.log('fired injectHTML');
+  const target = document.querySelector('#restaurant_list');
+  target.innerHTML = '';
 
-  const listEl = document.createElement("ol");
+  const listEl = document.createElement('ol');
   target.appendChild(listEl);
 
   list.forEach((item) => {
-    const el = document.createElement("li");
+    const el = document.createElement('li');
     el.innerText = item.name;
     listEl.appendChild(el);
   });
@@ -38,7 +38,7 @@ function injectHTML(list) {
       the usual ones are element.innerText and element.innerHTML
       Here's an article on the differences if you want to know more:
       https://developer.mozilla.org/en-US/docs/Web/API/Node/textContent#differences_from_innertext
-  
+
     ## What to do in this function
       - Accept a list of restaurant objects
       - using a .forEach method, inject a list element into your index.html for every element in the list
@@ -47,32 +47,40 @@ function injectHTML(list) {
 }
 
 function processRestaurants(list) {
-  console.log("fired restaurants list");
+  console.log('fired restaurants list');
   const range = [...Array(15).keys()]; // Special notation to create an array of 15 elements
   const newArray = range.map((item) => {
     const index = getRandomIntInclusive(0, list.length);
     return list[index];
   });
   return newArray;
-
   /*
       ## Process Data Separately From Injecting It
         This function should accept your 1,000 records
         then select 15 random records
         and return an object containing only the restaurant's name, category, and geocoded location
         So we can inject them using the HTML injection function
-  
+
         You can find the column names by carefully looking at your single returned record
         https://data.princegeorgescountymd.gov/Health/Food-Inspection/umjn-t2iz
-  
+
       ## What to do in this function:
-  
+
       - Create an array of 15 empty elements (there are a lot of fun ways to do this, and also very basic ways)
       - using a .map function on that range,
       - Make a list of 15 random restaurants from your list of 100 from your data request
       - Return only their name, category, and location
       - Return the new list of 15 restaurants so we can work on it separately in the HTML injector
     */
+}
+
+function filterList(list, filterInputValue) {
+  return list.filter((item) => {
+    if (!item.name) { return; }
+    const lowerCaseName = item.name.toLowerCase();
+    const lowerCaseQuery = filterInputValue.toLowerCase();
+    return lowerCaseName.includes(lowerCaseQuery);
+  });
 }
 
 async function mainEvent() {
@@ -84,17 +92,17 @@ async function mainEvent() {
     */
 
   // the async keyword means we can make API requests
-  const form = document.querySelector(".main_form"); // get your main form so you can do JS with it
-  const submit = document.querySelector("#get-resto"); // get a reference to your submit button
-  const loadAnimation = document.querySelector(".lds-ellipsis");
-  submit.style.display = "none"; // let your submit button disappear
+  const form = document.querySelector('.main_form'); // get your main form so you can do JS with it
+  const submit = document.querySelector('#get-resto'); // get a reference to your submit button
+  const loadAnimation = document.querySelector('.lds-ellipsis');
+  submit.style.display = 'none'; // let your submit button disappear
 
   /*
       Let's get some data from the API - it will take a second or two to load
       This next line goes to the request for 'GET' in the file at /server/routes/foodServiceRoutes.js
       It's at about line 27 - go have a look and see what we're retrieving and sending back.
      */
-  const results = await fetch("/api/foodServicePG");
+  const results = await fetch('/api/foodServicePG');
   const arrayFromJson = await results.json(); // here is where we get the data from our request as JSON
 
   /*
@@ -115,26 +123,33 @@ async function mainEvent() {
   );
 
   // This IF statement ensures we can't do anything if we don't have information yet
-  if (arrayFromJson.data?.length > 0) {
+  if (arrayFromJson.data?.length > 0) { // Return if we have no data
     // the question mark in this means "if this is set at all"
-    submit.style.display = "block"; // let's turn the submit button back on by setting it to display as a block when we have data available
+    submit.style.display = 'block'; // let's turn the submit button back on by setting it to display as a block when we have data available
 
     // Let's hide the load button no that we have some data to manipulate
-    loadAnimation.classList.remove("lds-ellipsis");
-    loadAnimation.classList.add("lds-ellipsis_hidden"); // Turns the submit button back on by setting it to display as a block when we have data available
+    loadAnimation.classList.remove('lds-ellipsis');
+    loadAnimation.classList.add('lds-ellipsis_hidden'); // Turns the submit button back on by setting it to display as a block when we have data available
+
+    let currentList = [];
+
+    form.addEventListener('input', (event) => {
+      console.log(event.target.value);
+      const filteredList = filterList(currentList, event.target.value);
+      injectHTML(filteredList);
+    });
 
     // And here's an eventListener! It's listening for a "submit" button specifically being clicked
     // this is a synchronous event event, because we already did our async request above, and waited for it to resolve
-    form.addEventListener("submit", (submitEvent) => {
+    form.addEventListener('submit', (submitEvent) => {
       // This is needed to stop our page from changing to a new URL even though it heard a GET request
       submitEvent.preventDefault();
 
       // This constant will have the value of your 15-restaurant collection when it processes
-      const restaurantList = processRestaurants(arrayFromJson.data);
-      console.log(restaurantList);
+      currentList = processRestaurants(arrayFromJson.data);
 
       // And this function call will perform the "side effect" of injecting the HTML list for you
-      injectHTML(restaurantList);
+      injectHTML(currentList);
 
       // By separating the functions, we open the possibility of regenerating the list
       // without having to retrieve fresh data every time
@@ -148,4 +163,4 @@ async function mainEvent() {
     It's calling the 'mainEvent' function at line 57
     It runs first because the listener is set to when your HTML content has loaded
   */
-document.addEventListener("DOMContentLoaded", async () => mainEvent()); // the async keyword means we can make API requests
+document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the async keyword means we can make API requests
